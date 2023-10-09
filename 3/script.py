e63012a08874ac62ac09f55d3d004edfd4dd16e8
Pndrii <andriy.pidgorodetsky@gmail.com>
2023-10-09 10:05:42 +0300

    new file:   3/script.py 	deleted:    firstry.py

---
+++ 3/script.py
@@ -0,0 +1,10 @@
+if (not False):
+    print(10 > 9)
+    print(10 == 9)
+    print(10 < 9)
+import random
+a = random.randint(0, 10)
+if a<=7 & a>=3:
+    print("a is in [3, 7]")
+else:
+    print("a is less then 3 or more then 7")
